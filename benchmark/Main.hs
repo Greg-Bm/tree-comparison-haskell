3f327a35af10a27338a66abc790e13df8aa47524
greg <placeholder@example.com>
2025-10-08 17:46:46 -0700

    improve test suite

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -13,14 +13,14 @@ import Tree
 import Tree.Avl as Avl
 import Tree.Bst as Bst
 import Tree.Rbt as Rbt
-import Tree.Workloads
+import Tree.Workloads (buildAndQuery)
 
 main :: IO ()
 main =
   defaultMain
-    [ mixedQueriesTests 1000 1000 100,
-      mixedQueriesTests 1000 20 100,
-      mixedQueriesTests 10000 10000 100
+    [ mixedQueriesTests 10000 100 100,
+      mixedQueriesTests 10000 1000 100,
+      buildAndQueryTests 1000 10000 1000 100
     ]
 
 mixedQueriesTests :: Int -> Int -> Int -> Benchmark
@@ -32,6 +32,24 @@ mixedQueriesTests length range trials =
         bench "rbt" $ nf (mixedQueriesTest @Rbt.Tree Proxy) list
       ]
 
+buildAndQueryTests init queries range trials =
+    env (pure (buildAndQueryTestInput init queries range trials)) $ \list ->
+    bgroup ("length " <> show init <> ", queries " <> show queries <> ", range " <> show range <> ", trials " <> show trials) [ bench "set" $ nf (buildAndQueryTest @Set.Set Proxy) list,
+        bench "avl" $ nf (buildAndQueryTest @Avl.Tree Proxy) list,
+        bench "bst" $ nf (buildAndQueryTest @Bst.Tree Proxy) list,
+        bench "rbt" $ nf (buildAndQueryTest @Rbt.Tree Proxy) list
+      ]
+
+buildAndQueryTest :: (SearchTree t, Ord a) => Proxy (t a) -> [([a], [a])] -> [([a], [Bool])]
+buildAndQueryTest proxy = fmap (uncurry (buildAndQuery proxy))
+
+buildAndQueryTestInput init queries range n = buildAndQueryInput init queries range <$> [1 .. n]
+
+buildAndQueryInput init queries range seed = do
+  let n1 = genRandomList init range seed
+      n2 = genRandomList queries range seed
+  (n1, n2)
+
 mixedQueriesTest :: (SearchTree t, Ord a) => Proxy (t a) -> [[(Action, a)]] -> [([Bool], t a)]
 mixedQueriesTest proxy = fmap (mixedQueries proxy)
 
@@ -41,11 +59,11 @@ mixedQueriesTestInput length range n = mixedQueriesInput length range <$> [1 .. 
 mixedQueriesInput :: Int -> Int -> Int -> [(Action, Int)]
 mixedQueriesInput length range seed = do
   let nums = genRandomList length range seed
-      actions = numToAction <$> genRandomList length 3 seed
+      actions = numToAction <$> genRandomList length 6 seed
       numToAction = \case
         1 -> Insert
         2 -> Delete
-        3 -> Member
+        _ -> Member
   zip actions nums
 
 genRandomList :: Int -> Int -> Int -> [Int]
